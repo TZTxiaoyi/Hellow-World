027a45f1485e47349602091f189be1d1a0722438
LiJinLiang <493372158@qq.com>
2017-06-16 19:58:40 +0800

    616购物车

---
+++ FoodControl/src/com/entity/LjlAddFood.java
@@ -0,0 +1,33 @@
+package com.entity;
+
+public class LjlAddFood {
+	private String foodname;
+	private String uprice;
+	private String number;
+	private String price;
+	public String getFoodname() {
+		return foodname;
+	}
+	public void setFoodname(String foodname) {
+		this.foodname = foodname;
+	}
+	public String getUprice() {
+		return uprice;
+	}
+	public void setUprice(String uprice) {
+		this.uprice = uprice;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+}
